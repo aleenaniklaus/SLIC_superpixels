e73ecd2e1674e912b91d15d1a97732b29b0ab757
Aleena Watson <29244427+aleenawatson@users.noreply.github.com>
2018-03-22 22:59:24 -0700

    Small updates to v2

--- slic.py
+++ slic.py
@@ -1,166 +1,159 @@
-import numpy as np
+import numpy
 import sys
 import cv2
+import tqdm
 
-class SLIC:
-    def __init__(self, img, step, nc):
-        self.img = img
-        self.height, self.width = img.shape[:2]
-        self._convertToLAB()
-        self.step = step
-        self.nc = nc
-        self.ns = step
-        self.FLT_MAX = 1000000
-        self.ITERATIONS = 14
+# using algorithm in 3.2 apply image gradients as computed in eq2:
+# G(x,y) = ||I(x+1,y) - I(x-1,y)||^2+ ||I(x,y+1) - I(x,y-1)||^2
 
-# cv2 function availabe to replace _convertoLAB fn
-    def _convertToLAB(self):
-        self.labimg = cv2.cvtColor(img, cv2.COLOR_BGR2LAB).astype(np.float64)
+def generatePixels():
+    indnp = numpy.mgrid[0:SLIC_height,0:SLIC_width].swapaxes(0,2).swapaxes(0,1)
+    for i in tqdm.tqdm(range(SLIC_ITERATIONS)):
+        SLIC_distances = SLIC_FLT_MAX * numpy.ones(SLIC_img.shape[:2])
+        for j in range(SLIC_centers.shape[0]):
+            xlow, xhigh = int(SLIC_centers[j][3] - SLIC_step), int(SLIC_centers[j][3] + SLIC_step)
+            ylow, yhigh = int(SLIC_centers[j][4] - SLIC_step), int(SLIC_centers[j][4] + SLIC_step)
 
+            if xlow <= 0:
+                xlow = 0
+            if xhigh > SLIC_width:
+                xhigh = SLIC_width
+            if ylow <=0:
+                ylow = 0
+            if yhigh > SLIC_height:
+                yhigh = SLIC_height
 
-    def generateSuperPixels(self):
-        self._initData()
-        indnp = np.mgrid[0:self.height,0:self.width].swapaxes(0,2).swapaxes(0,1)
-        for i in range(self.ITERATIONS):
-            self.distances = self.FLT_MAX * np.ones(self.img.shape[:2])
-            for j in range(self.centers.shape[0]):
-                xlow, xhigh = int(self.centers[j][3] - self.step), int(self.centers[j][3] + self.step)
-                ylow, yhigh = int(self.centers[j][4] - self.step), int(self.centers[j][4] + self.step)
+            cropimg = SLIC_labimg[ylow : yhigh , xlow : xhigh]
+            colordiff = cropimg - SLIC_labimg[int(SLIC_centers[j][4]), int(SLIC_centers[j][3])]
+            colorDist = numpy.sqrt(numpy.sum(numpy.square(colordiff), axis=2))
 
-                if xlow <= 0:
-                    xlow = 0
-                if xhigh > self.width:
-                    xhigh = self.width
-                if ylow <=0:
-                    ylow = 0
-                if yhigh > self.height:
-                    yhigh = self.height
+            yy, xx = numpy.ogrid[ylow : yhigh, xlow : xhigh]
+            pixdist = ((yy-SLIC_centers[j][4])**2 + (xx-SLIC_centers[j][3])**2)**0.5
+            dist = ((colorDist/SLIC_nc)**2 + (pixdist/SLIC_ns)**2)**0.5
 
-                cropimg = self.labimg[ylow : yhigh , xlow : xhigh]
-                colordiff = cropimg - self.labimg[int(self.centers[j][4]), int(self.centers[j][3])]
-                colorDist = np.sqrt(np.sum(np.square(colordiff), axis=2))
+            distanceCrop = SLIC_distances[ylow : yhigh, xlow : xhigh]
+            idx = dist < distanceCrop
+            distanceCrop[idx] = dist[idx]
+            SLIC_distances[ylow : yhigh, xlow : xhigh] = distanceCrop
+            SLIC_clusters[ylow : yhigh, xlow : xhigh][idx] = j
 
-                yy, xx = np.ogrid[ylow : yhigh, xlow : xhigh]
-                pixdist = ((yy-self.centers[j][4])**2 + (xx-self.centers[j][3])**2)**0.5
-                dist = ((colorDist/self.nc)**2 + (pixdist/self.ns)**2)**0.5
+        for k in range(len(SLIC_centers)):
+            idx = (SLIC_clusters == k)
+            colornp = SLIC_labimg[idx]
+            distnp = indnp[idx]
+            SLIC_centers[k][0:3] = numpy.sum(colornp, axis=0)
+            sumy, sumx = numpy.sum(distnp, axis=0)
+            SLIC_centers[k][3:] = sumx, sumy
+            SLIC_centers[k] /= numpy.sum(idx)
 
-                distanceCrop = self.distances[ylow : yhigh, xlow : xhigh]
-                idx = dist < distanceCrop
-                distanceCrop[idx] = dist[idx]
-                self.distances[ylow : yhigh, xlow : xhigh] = distanceCrop
-                self.clusters[ylow : yhigh, xlow : xhigh][idx] = j
+# At the end of the process, some stray labels may remain meaning some pixels
+# may end up having the same label as a larger pixel but not be connected to it
+# In the SLIC paper, it notes that these cases are rare, however this 
+# implementation seems to have a lot of strays depending on the inputs given
 
-            for k in range(len(self.centers)):
-                idx = (self.clusters == k)
-                colornp = self.labimg[idx]
-                distnp = indnp[idx]
-                self.centers[k][0:3] = np.sum(colornp, axis=0)
-                sumy, sumx = np.sum(distnp, axis=0)
-                self.centers[k][3:] = sumx, sumy
-                self.centers[k] /= np.sum(idx)
+def createConnectivity():
+    label = 0
+    adjlabel = 0
+    lims = int(SLIC_width * SLIC_height / SLIC_centers.shape[0])
+    
+    new_clusters = -1 * numpy.ones(SLIC_img.shape[:2]).astype(numpy.int64)
+    elements = []
+    for i in range(SLIC_width):
+        for j in range(SLIC_height):
+            if new_clusters[j, i] == -1:
+                elements = []
+                elements.append((j, i))
+                for dx, dy in [(-1,0), (0,-1), (1,0), (0,1)]:
+                    x = elements[0][1] + dx
+                    y = elements[0][0] + dy
+                    if (x>=0 and x < SLIC_width and 
+                        y>=0 and y < SLIC_height and 
+                        new_clusters[y, x] >=0):
+                        adjlabel = new_clusters[y, x]
+            count = 1
+            c = 0
+            while c < count:
+                for dx, dy in [(-1,0), (0,-1), (1,0), (0,1)]:
+                    x = elements[c][1] + dx
+                    y = elements[c][0] + dy
 
-    def _initData(self):
-        self.clusters = -1 * np.ones(self.img.shape[:2])
-        self.distances = self.FLT_MAX * np.ones(self.img.shape[:2])
+                    if (x>=0 and x<SLIC_width and y>=0 and y<SLIC_height):
+                        if new_clusters[y, x] == -1 and SLIC_clusters[j, i] == SLIC_clusters[y, x]:
+                            elements.append((y, x))
+                            new_clusters[y, x] = label
+                            count+=1
+                c+=1
+            if (count <= lims >> 2):
+                for c in range(count):
+                    new_clusters[elements[c]] = adjlabel
+                label-=1
+            label+=1
+    SLIC_new_clusters = new_clusters
 
-        centers = []
-        for i in range(self.step, self.width - int(self.step/2), self.step):
-            for j in range(self.step, self.height - int(self.step/2), self.step):
-                
-                nc = self._findLocalMinimum(center=(i, j))
-                color = self.labimg[nc[1], nc[0]]
-                center = [color[0], color[1], color[2], nc[0], nc[1]]
-                centers.append(center)
-        self.center_counts = np.zeros(len(centers))
-        self.centers = np.array(centers)
+def displayContours(color):
 
-    def createConnectivity(self):
-        label = 0
-        adjlabel = 0
-        lims = int(self.width * self.height / self.centers.shape[0])
-        dx4 = [-1, 0, 1, 0]
-        dy4 = [0, -1, 0, 1]
-        new_clusters = -1 * np.ones(self.img.shape[:2]).astype(np.int64)
-        elements = []
-        for i in range(self.width):
-            for j in range(self.height):
-                if new_clusters[j, i] == -1:
-                    elements = []
-                    elements.append((j, i))
-                    for dx, dy in zip(dx4, dy4):
-                        x = elements[0][1] + dx
-                        y = elements[0][0] + dy
-                        if (x>=0 and x < self.width and 
-                            y>=0 and y < self.height and 
-                            new_clusters[y, x] >=0):
-                            adjlabel = new_clusters[y, x]
-                count = 1
-                c = 0
-                while c < count:
-                    for dx, dy in zip(dx4, dy4):
-                        x = elements[c][1] + dx
-                        y = elements[c][0] + dy
+    isTaken = numpy.zeros(SLIC_img.shape[:2], numpy.bool)
+    contours = []
 
-                        if (x>=0 and x<self.width and y>=0 and y<self.height):
-                            if new_clusters[y, x] == -1 and self.clusters[j, i] == self.clusters[y, x]:
-                                elements.append((y, x))
-                                new_clusters[y, x] = label
-                                count+=1
-                    c+=1
-                if (count <= lims >> 2):
-                    for c in range(count):
-                        new_clusters[elements[c]] = adjlabel
-                    label-=1
-                label+=1
-        self.new_clusters = new_clusters
+    for i in range(SLIC_width):
+        for j in range(SLIC_height):
+            nr_p = 0
+            for dx, dy in [(-1,0), (-1,-1), (0,-1), (1,-1), (1,0), (1,1), (0,1), (-1,1)]:
+                x = i + dx
+                y = j + dy
+                if x>=0 and x < SLIC_width and y>=0 and y < SLIC_height:
+                    if isTaken[y, x] == False and SLIC_clusters[j, i] != SLIC_clusters[y, x]:
+                        nr_p += 1
 
-    def displayContours(self, color):
-        dx8 = [-1, -1, 0, 1, 1, 1, 0, -1]
-        dy8 = [0, -1, -1, -1, 0, 1, 1, 1]
+            if nr_p >= 2:
+                isTaken[j, i] = True
+                contours.append([j, i])
 
-        isTaken = np.zeros(self.img.shape[:2], np.bool)
-        contours = []
+    for i in range(len(contours)):
+        SLIC_img[contours[i][0], contours[i][1]] = color
 
-        for i in range(self.width):
-            for j in range(self.height):
-                nr_p = 0
-                for dx, dy in zip(dx8, dy8):
-                    x = i + dx
-                    y = j + dy
-                    if x>=0 and x < self.width and y>=0 and y < self.height:
-                        if isTaken[y, x] == False and self.clusters[j, i] != self.clusters[y, x]:
-                            nr_p += 1
-
-                if nr_p >= 2:
-                    isTaken[j, i] = True
-                    contours.append([j, i])
-
-        for i in range(len(contours)):
-            self.img[contours[i][0], contours[i][1]] = color
-
-    def _findLocalMinimum(self, center):
-        min_grad = self.FLT_MAX
-        loc_min = center
-        for i in range(center[0] - 1, center[0] + 2):
-            for j in range(center[1] - 1, center[1] + 2):
-                c1 = self.labimg[j+1, i]
-                c2 = self.labimg[j, i+1]
-                c3 = self.labimg[j, i]
-                if ((c1[0] - c3[0])**2)**0.5 + ((c2[0] - c3[0])**2)**0.5 < min_grad:
-                    min_grad = abs(c1[0] - c3[0]) + abs(c2[0] - c3[0])
-                    loc_min = [i, j]
-        return loc_min
+def findLocalMinimum(center):
+    min_grad = SLIC_FLT_MAX
+    loc_min = center
+    for i in range(center[0] - 1, center[0] + 2):
+        for j in range(center[1] - 1, center[1] + 2):
+            c1 = SLIC_labimg[j+1, i]
+            c2 = SLIC_labimg[j, i+1]
+            c3 = SLIC_labimg[j, i]
+            if ((c1[0] - c3[0])**2)**0.5 + ((c2[0] - c3[0])**2)**0.5 < min_grad:
+                min_grad = abs(c1[0] - c3[0]) + abs(c2[0] - c3[0])
+                loc_min = [i, j]
+    return loc_min
 
 img = cv2.imread(sys.argv[1])
 nr_superpixels = int(sys.argv[2])
-nc = int(sys.argv[3])
-
 step = int((img.shape[0]*img.shape[1]/nr_superpixels)**0.5)
 
-slic = SLIC(img, step, nc)
-slic.generateSuperPixels()
-slic.createConnectivity()
-slic.displayContours([0.0, 0.0, 0.0])
-cv2.imshow("superpixels", slic.img)
+SLIC_step = step
+SLIC_nc = int(sys.argv[3])
+SLIC_ns = step
+SLIC_FLT_MAX = 1
+SLIC_ITERATIONS = 6
+SLIC_img = img
+SLIC_height, SLIC_width = img.shape[:2]
+SLIC_labimg = cv2.cvtColor(img, cv2.COLOR_BGR2LAB).astype(numpy.float64)
+SLIC_clusters = -1 * numpy.ones(SLIC_img.shape[:2])
+SLIC_distances = SLIC_FLT_MAX * numpy.ones(SLIC_img.shape[:2])
+
+centers = []
+for i in range(SLIC_step, SLIC_width - int(SLIC_step/2), SLIC_step):
+    for j in range(SLIC_step, SLIC_height - int(SLIC_step/2), SLIC_step):
+        
+        nc = findLocalMinimum(center=(i, j))
+        color = SLIC_labimg[nc[1], nc[0]]
+        center = [color[0], color[1], color[2], nc[0], nc[1]]
+        centers.append(center)
+SLIC_center_counts = numpy.zeros(len(centers))
+SLIC_centers = numpy.array(centers)
+
+generatePixels()
+createConnectivity()
+displayContours([0.0, 0.0, 0.0])
+cv2.imshow("superpixels", SLIC_img)
 cv2.waitKey(0)
-cv2.imwrite("SLICimg.jpg", slic.img)
+cv2.imwrite("SLICimg.jpg", SLIC_img)
